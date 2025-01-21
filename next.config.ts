842befbb3d980d24e947806d900d9bf70642e216
quangpham-dev <quangpn.itedu@gmail.com>
2025-01-21 10:42:34 +0700

    Optimizing Fonts and Images

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // Optimize images
+  images: {
+    minimumCacheTTL: 60,
+
+    formats: ["image/avif", "image/webp"],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384]
+  }
   /* config options here */
 };
 
