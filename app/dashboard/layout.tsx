ce091034cf96900a752869959fce1e52489b13c4
quangpham-dev <quangpn.itedu@gmail.com>
2025-01-21 21:00:12 +0700

    Partial Prerendering

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,15 @@
 import SideNav from "@/app/ui/dashboard/sidenav";
 
+/**
+ * That's it. You may not see a difference in your application in development, 
+ * but you should notice a performance improvement in production. 
+ * Next.js will prerender the static parts of your route and defer the dynamic parts until the user requests them.
+ * The great thing about Partial Prerendering is that you don't need to change your code to use it. 
+ * As long as you're using Suspense to wrap the dynamic parts of your route, 
+ * Next.js will know which parts of your route are static and which are dynamic.
+ */
+export const experimental_ppr = true;
+
 // One benefit of using layouts in Next.js is that on navigation, only the page components update while the layout won't re-render.
 // This is called partial rendering which preserves client-side React state in the layout when transitioning between pages.
 export default function Layout({ children }: { children: React.ReactNode }) {
