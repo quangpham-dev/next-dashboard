6d4d84c10991fbf6677463e47d56f25e5e788d3b
quangpham-dev <quangpn.itedu@gmail.com>
2025-01-21 16:11:24 +0700

    Static and Dynamic Rendering

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -7,7 +7,14 @@ import {
   fetchLatestInvoices,
   fetchRevenue
 } from "@/app/lib/data";
-
+/**
+ * With dynamic rendering, content is rendered on the server for each user at request time (when the user visits the page).
+ * There are a couple of benefits of dynamic rendering:
+ * Real-Time Data - Dynamic rendering allows your application to display real-time or frequently updated data. This is ideal for applications where data changes often.
+ * User-Specific Content - It's easier to serve personalized content, such as dashboards or user profiles, and update the data based on user interaction.
+ * Request Time Information - Dynamic rendering allows you to access information that can only be known at request time, such as cookies or the URL search parameters.
+ * With dynamic rendering, your application is only as fast as your slowest data fetch.
+ */
 export default async function Page() {
   const revenue = await fetchRevenue();
   const latestInvoices = await fetchLatestInvoices();
