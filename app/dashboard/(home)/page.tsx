d0ce0a9c3e515befed5aa16c0917dd851bc6be86
Quang Pham <quangpn.itedu@gmail.com>
2025-01-22 15:03:08 +0700

    Improving Accessibility (#12)

--- app/dashboard/(home)/page.tsx
+++ app/dashboard/(home)/page.tsx
@@ -2,7 +2,6 @@ import CardWrapper from "@/app/ui/dashboard/cards";
 import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import { lusitana } from "@/app/ui/fonts";
-import { fetchCardData } from "@/app/lib/data";
 import { Suspense } from "react";
 import {
   RevenueChartSkeleton,
@@ -19,13 +18,6 @@ import {
  * With dynamic rendering, your application is only as fast as your slowest data fetch.
  */
 export default async function Page() {
-  const {
-    numberOfCustomers,
-    numberOfInvoices,
-    totalPaidInvoices,
-    totalPendingInvoices
-  } = await fetchCardData();
-
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
